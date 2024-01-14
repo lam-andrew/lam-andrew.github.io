d611a51e57bbe80c33d0b562e851449956c8810c
lam-andrew <andrewlam2193@gmail.com>
2024-01-13 17:29:44 -0800

    feat: overlap text styling

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,17 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const overlapEls = document.querySelectorAll(".overlap")
+|| [];
+
+overlapEls.forEach((el) => {
+    const chars = [...el.textContent];
+    el.innerHTML = "";
+    chars.forEach((char, index) => {
+        const span = document.createElement("span");
+        span.textContent = char;
+        span.style.setProperty("--index", index);
+        el.append(span);
+    });
+});
